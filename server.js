506ba351edbb35effa187a0cd8885d8edfd7fb5f
murphbra <murphbra@oregonstate.edu>
2021-10-29 17:00:08 -0700

    update

--- server.js
+++ server.js
@@ -151,10 +151,9 @@ function delete_boat(id) {
 /* ------------- End Model Functions ------------- */
 
 /* ------------- Begin Controller Functions ------------- */
-router.get('/', function(req, res){
+router.get('/index', function(req, res){
     res.send('hello world'); 
-})
-
+}); 
 
 
 router.get('/boats', function (req, res) {
