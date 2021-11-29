d6bbf0642f58ca88ed367fa44ef93dafbc4272a2
murphbra <murphbra@oregonstate.edu>
2021-11-29 15:03:26 -0800

    update

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.engine('.hbs', exphbs({
 }));
 app.use(express.json()); 
 app.use(express.urlencoded({extended:true}));
-app.use(express.static(path.join(__dirname, 'public')); 
+app.use(express.static(path.join(__dirname, 'public'))); 
 app.set('view engine', '.hbs'); 
 
 
